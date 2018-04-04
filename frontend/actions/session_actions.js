5367197c31a682a873c44fc04b12c817cfcb0090
Isis Moran <imoran@mail.ccsf.edu>
2018-04-04 11:23:57 -0700

    Add appropriate session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,36 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then(signedUpUser => (
+    dispatch(receiveCurrentUser(signedUpUser))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then(loggedInUser => (
+     dispatch(receiveCurrentUser(loggedInUser))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then(loggedOutUser => (
+    dispatch(receiveCurrentUser(null))
+  ))
+);
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -7,5 +7,8 @@ import logout from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
   ReactDOM.render(<h1>wat</h1>, root);
 });
