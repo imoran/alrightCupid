cbb87b78714dfc7e8e0ce49b5000f18d0326285b
Isis Moran <imoran@mail.ccsf.edu>
2018-04-03 17:38:27 -0700

    Fix Routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,15 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render 'api/users/index'
+  end
+
   def create
     @user = User.new(user_params)
 
     if @user.save
       login(@user)
-      render 'Sign up successful!'
+      render 'api/users/show'
     else
       render json: @user.errors.full_messages, status: 422
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/root'
+
   namespace :api, defaults: {format: :json} do
-    resource :users, only: [:create]
+    resources :users, only: [:index, :create]
     resource :session, only: [:create, :destroy]
   end
+  root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
